e55a82607d2292f414272a1c52e0daf888a162af
DESKTOP-PVQB0H3\abhia <abhistreamslive@gmail.com>
2021-07-27 16:36:17 +0530

    Basic Sample Plugin

---
+++ src/Main.php
@@ -0,0 +1,26 @@
+<?php
+namespace src;
+use pocketmine\plugin\pluginbase;
+
+use pocketmine\command\command;
+use pocketmine\command\CommandSender;
+use pocketmine\player;
+
+class Main extends pluginbase{
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool[
+               switch($cmd->getName()){
+                 case "hi":
+                 if($sender instanceof Player){
+                   $sender->sendMessage("§a Test Message");
+              }
+              break;
+
+            }
+
+               }
+              return true;
+
+
+             ]
+
+}
